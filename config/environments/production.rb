0d209cbcbfe062b83bd15627e668418aa0eb0114
Roonglit Chareonsupkul <roongit@gmail.com>
2018-07-17 15:01:40 +0700

    update production setting to upload image to S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,9 +41,9 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
   # To use ActiveStorage with S3 remove the above and comment in the following line:
-  # config.active_storage.service = :amazon
+  config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
